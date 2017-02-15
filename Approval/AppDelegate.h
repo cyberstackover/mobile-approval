a824bf3ca20251c2c066dd157e90407f73b8be94
Dody <dodyrw@gmail.com>
2017-02-15 12:47:45 +0700

    Initial Commit

---
+++ Approval/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Approval
+//
+//  Created by Dody Rachmat Wicaksono on 2/15/17.
+//  Copyright © 2017 Semen Indonesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Approval/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Approval
+//
+//  Created by Dody Rachmat Wicaksono on 2/15/17.
+//  Copyright © 2017 Semen Indonesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
