5e5d90867e2d13c5a54e3f74b60d914f46c39077
dody <dodyrw@gmail.com>
2017-06-13 05:29:14 +0700

    afnetworking logger di console

--- Approval/AppDelegate.m
+++ Approval/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "AFNetworkActivityLogger.h"
+#import "AFNetworkActivityConsoleLogger.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +19,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    AFNetworkActivityConsoleLogger *logger = [AFNetworkActivityLogger sharedLogger].loggers.anyObject;
+    logger.level = AFLoggerLevelDebug;
+    [[AFNetworkActivityLogger sharedLogger] startLogging];
     
     [[UINavigationBar appearance] setBarStyle:UIStatusBarStyleLightContent];
     
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ target 'Approval' do
   pod 'IQKeyboardManager'
   pod 'SVProgressHUD'
   pod 'AFNetworking'
+  pod 'AFNetworkActivityLogger', git: 'https://github.com/AFNetworking/AFNetworkActivityLogger.git', branch: '3_0_0'
 end
